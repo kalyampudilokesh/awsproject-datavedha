fbb696ba112b2daf2e5e0050c1cff93b0d5e425f
Loki <lkalyampudi@gmail.com>
2026-06-23 13:13:39 +0530

    allowing origins

--- orders-service/main.py
+++ orders-service/main.py
@@ -44,8 +44,8 @@ app = FastAPI(
 # CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=settings.ALLOWED_ORIGINS.split(","),
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
